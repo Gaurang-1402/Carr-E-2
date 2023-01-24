e61c85d0a3de44f4a16134b8918bc178f7e461c6
“gaurang-1402” <gr2159@nyu.edu>
2023-01-24 05:47:28 +0530

    clear out some files and folders

--- build/carr-e-2/CMakeFiles/carr-e-2_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/carr-e-2_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/carr-e-2_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
